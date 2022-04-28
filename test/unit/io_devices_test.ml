fa464ad64143e87fdc46e000211b9bce331488ca
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-28 17:29:52 +0200

    add configuration test file
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- test/unit/io_devices_test.ml
+++ test/unit/io_devices_test.ml
@@ -1,3 +1,3 @@
 open OUnit2
 
-let init_io_devices_test = assert_equal 0 0
+let init_io_devices_test = assert_equal 0 0 ~printer:string_of_int
--- test/unit/lis2022.ml
+++ test/unit/lis2022.ml
@@ -2,6 +2,10 @@ open OUnit2
 
 let tests =
   "lis2022 unit test"
-  >::: [ ("io_deviced" >:: fun _ -> Io_devices_test.init_io_devices_test) ]
+  >::: [
+         ("io_deviced" >:: fun _ -> Io_devices_test.init_io_devices_test);
+         ( "configuration" >:: fun _ ->
+           Configuration_test.init_configuration_test );
+       ]
 
 let _ = run_test_tt_main tests
