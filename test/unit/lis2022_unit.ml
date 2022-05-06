616dcbf4ae1b1a63acb99f2b549047d4c18310b7
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-05-06 15:43:17 +0200

    tests: integration testing
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- test/unit/lis2022_unit.ml
+++ test/unit/lis2022_unit.ml
@@ -3,7 +3,8 @@ open OUnit2
 let tests =
   "lis2022 unit test"
   >::: [
-         ("io_deviced" >:: fun _ -> Io_devices_test.init_io_devices_test);
+         ("io_device_init" >:: fun _ -> Io_devices_test.init_io_devices_test);
+         ("io_device_advance" >:: fun _ -> Io_devices_test.io_device_advance_one);
          ( "configuration" >:: fun _ ->
            Configuration_test.init_configuration_test );
        ]
