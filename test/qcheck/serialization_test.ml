1afc8a638501e66b2b809213b6360e7166f5fa98
iwilare <iwilare@gmail.com>
2022-06-06 17:44:31 +0200

    Fix instruction and memory tests name

--- test/qcheck/serialization_test.ml
+++ test/qcheck/serialization_test.ml
@@ -5,20 +5,20 @@ open Generators
 let test_enc_dec_of_registers =
   let property i = reg_encode i |> reg_decode = Some i in
   let gen = Register.any_register in
-  QCheck2.Test.make ~name:"Register encode decode is involutive" ~count:20000 gen
+  QCheck2.Test.make ~name:"Register decode the inverse of encode" ~count:20000 gen
     property
 
 let test_enc_dec_of_instructions =
   let property i = encode i |> decode = Some i in
   let gen = Instructions.instr in
-  QCheck2.Test.make ~name:"Instruction encode decode is involutive" ~count:20000
+  QCheck2.Test.make ~name:"Instruction decode is the inverse of encode" ~count:20000
   ~print:Lis2022.Instr.string_of_instr
     gen property
 
 let test_enc_dec_of_instructions_in_memory =
   let property (m, a, i) = m |> encode_and_put a i |> fetch_and_decode a = Some i in
   let gen = QCheck2.Gen.triple Memory.memory Memory.address Instructions.instr in
-  QCheck2.Test.make ~name:"Instruction encode decode in memory is involutive" ~count:20000
+  QCheck2.Test.make ~name:"Instruction decode in memory is the inverse of encode in memory" ~count:20000
   ~print:(fun (m, a, i) -> Lis2022.Instr.string_of_instr i ^ "\n" ^ string_of_memory (m |> encode_and_put a i))
   gen property
 
